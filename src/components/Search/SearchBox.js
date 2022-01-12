2f3ec42a49f74de1082334208857cf3fcf3f1c5f
RegR2 <88548734+RegR2@users.noreply.github.com>
2022-01-12 14:05:26 +0200

    WIP

--- src/components/Search/SearchBox.js
+++ src/components/Search/SearchBox.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Paper, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 export default function SearchBox(props) {
   const { setResults } = props;
+  const [hobbies, setHobbies] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -16,22 +22,56 @@ export default function SearchBox(props) {
     setResults(res);
   };
 
+  const handleChange = (event) => {};
+
   return (
     <Container>
       {" "}
       <Box
         component="form"
         onSubmit={handleSubmit}
-        sx={{ bgcolor: "primary.main", radius: "2rem" }}
+        sx={{ bgcolor: "primary.main", borderRadius: "1rem" }}
       >
         <Typography
           variant="h6"
-          sx={{ margin: "1rem 1rem 1rem 2rem", color: "white" }}
+          sx={{
+            margin: "2rem 1rem 0 2rem",
+            paddingTop: ".5rem",
+            color: "white",
+          }}
         >
-          Route Search balhadsfhalsdkfjal
+          Route Search
         </Typography>
-        <TextField sx={{ bgcolor: "white", margin: "1rem", width: "22rem" }} />
-        <TextField sx={{ bgcolor: "white", margin: "1rem", width: "20rem" }} />
+        <TextField
+          sx={{
+            margin: "1rem",
+            width: "20rem",
+            bgcolor: "white",
+            borderRadius: "0.5rem",
+          }}
+          variant="outlined"
+        />
+        <FormControl
+          sx={{
+            margin: "1rem",
+            width: "20rem",
+            bgcolor: "white",
+            borderRadius: "0.5rem",
+          }}
+        >
+          <InputLabel id="demo-simple-select-label">Hobbies</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={hobbies}
+            label="Hobbies"
+            onChange={handleChange}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
       </Box>
     </Container>
   );
