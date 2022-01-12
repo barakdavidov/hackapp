7f04d34a4b209f2ead43d02be847344cbe721837
RegR2 <88548734+RegR2@users.noreply.github.com>
2022-01-12 11:18:21 +0200

    bottom navbar finished

--- src/components/signin.js
+++ src/components/signin.js
@@ -11,7 +11,6 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import InfoContext from "./InfoContext";
 import { Navigate, useNavigate } from "react-router-dom";
 
@@ -32,8 +31,6 @@ function Copyright(props) {
     </Typography>
   );
 }
-
-const theme = createTheme();
 
 export default function SignIn({ signIn }) {
   const { user, setUser } = useContext(InfoContext);
@@ -84,120 +81,113 @@ export default function SignIn({ signIn }) {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            {login ? "Sign in" : "Sign up"}
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            {!login && (
-              <>
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoComplete="first name"
-                  value={userInfo.firstName}
-                  onChange={handleChange}
-                  autoFocus={!login}
-                />
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  autoComplete="last name"
-                  value={userInfo.lastName}
-                  onChange={handleChange}
-                />
-              </>
-            )}
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              autoComplete="email"
-              value={userInfo.email}
-              onChange={handleChange}
-              autoFocus={login}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete={login ? "current-password" : "password"}
-              value={userInfo.password}
-              onChange={handleChange}
-            />
-            {!login && (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          {login ? "Sign in" : "Sign up"}
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          {!login && (
+            <>
               <TextField
                 margin="normal"
                 required
                 fullWidth
-                id="confirmation"
-                label="Confirm Password"
-                autoComplete="password confirmation"
-                value={userInfo.confirmation}
+                id="firstName"
+                label="First Name"
+                autoComplete="first name"
+                value={userInfo.firstName}
+                onChange={handleChange}
+                autoFocus={!login}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                autoComplete="last name"
+                value={userInfo.lastName}
                 onChange={handleChange}
               />
-            )}
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
+            </>
+          )}
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            autoComplete="email"
+            value={userInfo.email}
+            onChange={handleChange}
+            autoFocus={login}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete={login ? "current-password" : "password"}
+            value={userInfo.password}
+            onChange={handleChange}
+          />
+          {!login && (
+            <TextField
+              margin="normal"
+              required
               fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              {login ? "Sign In" : "Sign Up"}
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                {login && (
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                )}
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2" onClick={() => setLogin(!login)}>
-                  {login
-                    ? "Don't have an account? Sign Up"
-                    : "Already have and account? Sign In"}
+              id="confirmation"
+              label="Confirm Password"
+              autoComplete="password confirmation"
+              value={userInfo.confirmation}
+              onChange={handleChange}
+            />
+          )}
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            {login ? "Sign In" : "Sign Up"}
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              {login && (
+                <Link href="#" variant="body2">
+                  Forgot password?
                 </Link>
-              </Grid>
+              )}
             </Grid>
-          </Box>
+            <Grid item>
+              <Link href="#" variant="body2" onClick={() => setLogin(!login)}>
+                {login
+                  ? "Don't have an account? Sign Up"
+                  : "Already have and account? Sign In"}
+              </Link>
+            </Grid>
+          </Grid>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
+      </Box>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </Container>
   );
 }
